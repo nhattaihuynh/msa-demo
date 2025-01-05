c02c8503c3f9b966e89851a530170fdab4780843
taihuynh <nhattaihuynh@gmail.com>
2025-01-05 19:42:28 +0700

    init customer-service

---
+++ e-commerce-app/services/customer-service/src/test/java/com/taihuynh/ecommerce/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taihuynh.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
